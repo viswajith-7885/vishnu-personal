e1723cb4eb290636e049b4a7ee6fe3382f2e80e9
viswajith-7885 <viswajith21295@gmail.com>
2024-06-06 20:30:05 +0530

    fifth commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -235,10 +235,6 @@
   new Swiper('.portfolio-details-slider', {
     speed: 400,
     loop: true,
-    autoplay: {
-      delay: 5000,
-      disableOnInteraction: false
-    },
     pagination: {
       el: '.swiper-pagination',
       type: 'bullets',
